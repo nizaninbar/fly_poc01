f181afc5494ad5e6246c55e287059c8d40f9a8d1
Nizan Inbar <nizanin@matrix.co.il>
2020-02-22 14:56:36 +0200

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>FLY</h1>
+    <h2>v.0001</h2>
+    <p>
+      Front end layer, demonstrating how CRM Dynamics components,<br />
+      can be shown and manipulate in a modern Single Page Application.
+    </p>
   </div>
 </template>
